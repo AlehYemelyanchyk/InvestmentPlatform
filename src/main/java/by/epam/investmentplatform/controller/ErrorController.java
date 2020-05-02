0a44b1ca3d906678a2f5516437fbc71d2fa98725
Aleh Yemelyanchyk <yem1403@gmail.com>
2020-05-02 19:43:45 +0300

    Added ErrorServlet.

---
+++ src/main/java/by/epam/investmentplatform/controller/ErrorController.java
@@ -0,0 +1,20 @@
+package by.epam.investmentplatform.controller;
+
+import by.epam.investmentplatform.controller.command.JspPageName;
+import by.epam.investmentplatform.util.RoutingUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/errorServlet")
+public class ErrorController extends AbstractController {
+    private static final long serialVersionUID = -31640364473886345L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        RoutingUtils.forwardToPage(JspPageName.ERROR_PAGE, req, resp);
+    }
+}
